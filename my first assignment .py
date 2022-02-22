8084cfe9e1a729c372a4260f0c3bddff6311ba90
Harinandankumar <97117217+Harinandankumar@users.noreply.github.com>
2022-02-22 09:03:38 +0530

    first assignment

---
+++ my first assignment .py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* 
+'hello'
+-87.8
+- 
+/ 
++	
+6 
+
+ans:-
+    * - Expression
+    'hello' - Value
+    -87.8 - Value
+    - -Expression
+    /- Exprission 
+    + - Exprission
+    6 - Value
+    2.What is the difference between string and variable?
+
+ans:- 
+    String :- Astring can be written in quates such : "apple",'apple'.
+    Variable:- it is the thing in which we can store data just like a container.3.Describe three different data types.
+
+ans:- 
+    Integer:- Integer can store only numerical values such as 2,3,5 etc.
+    Float:- Float can store flooting values such as 233.33,22.5 etc.
+    Booleans:- Booleans can store only true/False values.
+        4.What is an expression made up of? What do all expressions do?
+
+ans:-
+    Expression is made up of values containers and mathematical operators(operands)
+    eg:- a=2+3 and expressions are used to evalute the values or represent the rusult on the screen.
+        5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+ans:-
+     Expression is made up of values containers and mathematical operators(operands) and the statement is just like a command that a python interpreter executes like print.
+            6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+
+ans:-
+    it gives 23 as execution code. 
+# In[1]:
+
+
+bacon=22
+
+
+# In[2]:
+
+
+bacon+1
+
+7.  What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3
+
+ans:-  
+     it will be the same.
+     
+# In[3]:
+
+
+'spam'+'spamspam'
+
+
+# In[2]:
+
+
+'spam'*3
+
+8.Why is eggs a valid variable name while 100 is invalid?
+
+ans:-
+    Because we can not start giving variable an integer name if we, we should begin with a string like alphabet name them integer. e100 or eggs100 is valid.9. What three functions can be used to get the integer, floating-point number, or string version of a value.
+
+ans:-
+    str(),int(),float().10.Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+
+ans:-
+    Because 99 is an integer it can not be concatenated with strings , if we have to concatenate it we need to do typecasting.
